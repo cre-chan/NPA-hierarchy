4718119d0cde550a7c6b2a812fa17fb6f4f5524d
cre-chan <xu.duo.x3@s.mail.nagoya-u.ac.jp>
2025-08-21 18:51:01 +0900

    We added the function to write a SDP program to SDPA sparse format (*.dat-s). We implement a script run-sdpa.py to read  SDPA files and solve the probolem.

---
+++ run-sdpa.py
@@ -0,0 +1,28 @@
+import sdpap
+import sys
+
+
+usage ="""
+python path_to_input_file path_to_output_file
+"""
+
+class SolverArgs:
+    def __init__(self):
+        prob_filename, result_filename = sys.argv[1:3]
+        self.infile = prob_filename
+        self.outfile = result_filename
+        print(prob_filename,result_filename)
+
+    @property
+    def sdpopt(self):
+        return {
+            "print":"no",
+            "sdpaResult":self.outfile
+        }
+
+
+if __name__ == "__main__":
+    args = SolverArgs()
+    filename = args.infile
+    A, b, c, K, J = sdpap.importsdpa(filename)
+    x, y, sdpapinfo, timeinfo, sdpainfo = sdpap.solve(A,b,c,K,J,args.sdpopt)
