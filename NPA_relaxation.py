0be747d771f3751256d804f7b8b09804b594108a
cre-chan <xu.duo.x3@s.mail.nagoya-u.ac.jp>
2025-08-23 10:18:50 +0900

    ADD: We add a class for the Magic Square Game

--- NPA_relaxation.py
+++ NPA_relaxation.py
@@ -32,47 +32,67 @@ class CHSH:
                 "objective" : 1,
                 "equalities": [self.gpoly - prob]
                 }
+    
+class MSG:
+    def __init__(self):
+        F = ncr.generate_operators('F',3, hermitian=True)
+        G = ncr.generate_operators('G',3,hermitian=True)
+        GPoly = (9/2 + 1/2 * F[0] *G[0] + 1/2 * F[1] *G[0] \
+            + 1/2 * F[2] * G[0] + 1/2 * F[0] * G[1] \
+            + 1/2 * F[1] * G[1] + 1/2 * F[2] * G[1] \
+            + 1/2 * F[0] * G[2] + 1/2 * F[1] * G[2] \
+            + 1/2 * F[2] * G[2])/9
+
+        Substitutions = {
+        op**2: 1 for op in F+G
+        } | {
+            f*g:g*f for f in F for g in G 
+        }
+        self.vars = F+G
+        self.substituions = Substitutions
+        self.gpoly = GPoly
+
+    def get_relaxation(self):
+        return ncr.SdpRelaxation(self.vars)
+
+    def get_maximum_violation(self):
+        return {
+                "substitutions":self.substituions,
+                "objective" : -self.gpoly
+                }
+    
+    def get_check_prob(self,prob=1):
+        return {
+                "substitutions":self.substituions,
+                "objective" : 1,
+                "equalities": [self.gpoly - prob]
+                }
 
 if __name__ == "__main__":
     mode = sys.argv[1]
 
-    level = 1
-    [free] = ncr.generate_variables("f")
-    F = ncr.generate_operators('F',3, hermitian=True)
-    G = ncr.generate_operators('G',3,hermitian=True)
-    Poly = 9/2 + 1/2 * F[0] *G[0] + 1/2 * F[1] *G[0] + 1/2 * F[2] * G[0] + 1/2 * F[0] * G[1] + 1/2 * F[1] * G[1] + 1/2 * F[2] * G[1] + 1/2 * F[0] * G[2] + 1/2 * F[1] * G[2] + 1/2 * F[2] * G[2]
-
-    Substitutions = {
-        op**2: 1 for op in F+G
-    } | {
-        f*g:g*f for f in F for g in G 
-    }
+    level = 3
+    
+    msg = MSG()
 
     if mode == "--solve":
-        sdp = ncr.SdpRelaxation(F+G)
+        sdp = msg.get_relaxation()
         sdp.get_relaxation(level, 
-                        objective=-Poly,
-                        substitutions=Substitutions)
+                        **msg.get_maximum_violation())
 
         # sdp.write_to_file("MSG-1.csv")
         # sdp.write_to_file("MSG-1.dat-s")
         sdp.solve()
 
-        print("The primial optimal value: ",-sdp.primal/9)
+        print("The primial optimal value: ",sdp.primal)
     elif mode == "--check":
-        sdp = ncr.SdpRelaxation(F+G)
-        sdp.get_relaxation(
-            level,
-            objective=1,
-            substitutions=Substitutions,
-            equalities=[
-               Poly/9 - 5 
-                ]
-        )
-
+        sdp = msg.get_relaxation()
+        sdp.get_relaxation(level, 
+                        **msg.get_check_prob(1))
         sdp.solve()
         
-        print("Does Alice and Bob win the game with probability 1? ",sdp.status)
+        print("Does Alice and Bob win the game with probability 1? ", 
+              sdp.status.find("optimal")!=-1)
         print(sdp.primal)
     else:
         raise ValueError("sss")
